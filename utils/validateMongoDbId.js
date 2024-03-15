971da56c469a11460f4a05be5182c11e74296161
Yash Kothari <kothariyash2711@gmail.com>
2024-03-15 18:39:09 +0530

    idk

--- utils/validateMongoDbId.js
+++ utils/validateMongoDbId.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const validateMongoDbId = (id) => {
+  // Check if the provided ID is a valid MongoDB ObjectID
   const isValid = mongoose.Types.ObjectId.isValid(id);
-  if (!isValid) throw new Error("This id is not valid or not Found");
+  
+  // If the ID is not valid, throw an error
+  if (!isValid) {
+    throw new Error("This id is not valid or not found");
+  }
 };
+
 module.exports = validateMongoDbId;
