2d6d0618a27ed0f82f59483c5dc17fce7e4fcc33
Kalpesh <kalpeshdhoke7@gmail.com>
2024-03-26 16:43:50 +0530

    change wishlist remove method from put to delete

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -16,7 +16,7 @@ router.post("/", authMiddleware, isAdmin, createProduct);
 
 router.get("/:id", getaProduct);
 router.put("/wishlist", authMiddleware, addToWishlist);
-router.put("/wishlist/remove", authMiddleware, removeFromWishlist);
+router.delete("/wishlist", authMiddleware, removeFromWishlist);
 router.put("/rating", authMiddleware, rating);
 
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
