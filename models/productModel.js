2e5ef03adadf9edcef2e3d5caf8c86ec16b8f9cd
Kalpesh <kalpeshdhoke7@gmail.com>
2024-04-24 18:27:39 +0530

    revert back to commit

--- models/productModel.js
+++ models/productModel.js
@@ -28,11 +28,6 @@ var productSchema = new mongoose.Schema(
       required: true,
     },
     images: [{
-      id: {
-        type: mongoose.Schema.Types.ObjectId,
-        default: mongoose.Types.ObjectId,
-        required: true
-      },
       url: String
     }],
     color: {
