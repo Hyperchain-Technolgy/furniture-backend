ff617936ddc58abb9daf0d8c38983e87c27ec84f
Yash Kothari <kothariyash2711@gmail.com>
2024-05-31 16:57:29 +0530

    change in upload file

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,26 +1,18 @@
-const express = require('express');
-const { uploadImages } = require('../controller/uploadCtrl');
-const { createProduct } = require('../controller/productCtrl');
-const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
-const { uploadPhoto, productImgResize } = require('../middlewares/uploadImage');
+const express = require("express");
+const { uploadImages, deleteImages } = require("../controller/uploadCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const { uploadPhoto, productImgResize } = require("../middlewares/uploadImage");
 const router = express.Router();
 
 router.post(
-  '/',
+  "/",
   authMiddleware,
   isAdmin,
-  uploadPhoto,
+  uploadPhoto.array("images", 10),
   productImgResize,
   uploadImages
 );
 
-router.post(
-  '/',
-  authMiddleware,
-  isAdmin,
-  uploadPhoto,
-  productImgResize,
-  createProduct
-);
+router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
 
 module.exports = router;
